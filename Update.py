fa835aaccf9fde39a53024f3f47e115545d5f70e
Moonquakes-liu <115360761+Moonquakes-liu@users.noreply.github.com>
2022-10-18 20:33:37 +0800

    Update Update.py

--- Update.py
+++ Update.py
@@ -3,7 +3,8 @@ import os
 
 
 def replace(name, tar_mess, replace_mess):
-    file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), name)
+    file_name = os.path.join(os.path.dirname(os.path.abspath(__file__))[:-10], name)
+    print(file_name)
     with open(file_name, "r", encoding="utf-8") as F:
         lines = F.readlines()
     with open(file_name, "w", encoding="utf-8") as f_w:
