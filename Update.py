fb5ea3590a559b2086d8f87b694f47942aa73338
Moonquakes-liu <115360761+Moonquakes-liu@users.noreply.github.com>
2023-02-02 20:11:39 +0800

    Update Update.py

--- Update.py
+++ Update.py
@@ -195,6 +195,7 @@ def Update():
                     update_info.append('%s: %s' % (Operate_File, parser[info]['INFORMATION']))
         except KeyError:
             pass
+    shutil.rmtree(os.path.join(root_path, '__Update__'))  # 更新完成，删除相关文件记录
     if update_info:
         print(' ->->', '\033[0;33m更新内容:\033[0m')
         for i in update_info:
@@ -204,7 +205,6 @@ def Update():
                        'You can recovery it here!\033[0m')
     if update_info:
         print(' ->->', '\033[0;32mUpdate complete, Please Restart the program!\033[0m')
-        shutil.rmtree(os.path.join(root_path, '__Update__')), ZF.close()  # 更新完成，删除相关文件记录
         sys.exit()
         
 
